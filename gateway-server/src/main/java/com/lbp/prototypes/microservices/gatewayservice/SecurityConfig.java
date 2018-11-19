3e4d95d497207a071307b32ce5ad8707a4d9c7ad
ajohnson <Adam.Johnson@rbaconsulting.com>
2018-11-19 15:32:11 -0600

    Updated loosened security at bit to focuse testing on spring session, also removed book services to realign to another service later

--- gateway-server/src/main/java/com/lbp/prototypes/microservices/gatewayservice/SecurityConfig.java
+++ gateway-server/src/main/java/com/lbp/prototypes/microservices/gatewayservice/SecurityConfig.java
@@ -1,38 +1,51 @@
-package com.rba.prototypes.microservices.namingservice;
+package com.lbp.prototypes.microservices.ratingserver;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 
 /**
  * @author Adam Johnson <Adam.Johnson@rbaconsulting.com>
- * @since 11/19/2018
+ * @since 11/13/2018
  */
 @Configuration
 @EnableWebSecurity
+@Order(1)
 public class SecurityConfig extends WebSecurityConfigurerAdapter
 {
+
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception
     {
-        UserDetails user = User.withDefaultPasswordEncoder().username("discuser").password("discPassword").roles("SYSTEM").build();
-
-        auth.inMemoryAuthentication().withUser(user);
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("password")
+                .roles("USER")
+                .and()
+                .withUser("admin")
+                .password("admin")
+                .roles("ADMIN");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception
     {
-        http
-                .csrf()
+        http.httpBasic()
                     .disable()
-                .authorizeRequests().anyRequest().authenticated()
-                .and().httpBasic();
+                .authorizeRequests()
+                    .antMatchers("/ratings")
+                    .hasRole("USER")
+                    .antMatchers("/ratings/all")
+                    .hasAnyRole("USER", "ADMIN")
+                    .anyRequest()
+                    .authenticated()
+                    .and()
+                .csrf()
+                    .disable();
     }
-
 }
